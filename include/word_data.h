6ea58f6b2cf4982b5daa267d992e19b22e81e710
Peter Vize <4411615+pvize1@users.noreply.github.com>
2021-01-24 21:31:51 +0000

    Change design of word_list
    Change design from word_list class to vector of word_data classes
    Added blank word_data class

---
+++ include/word_data.h
@@ -0,0 +1,16 @@
+#ifndef WORD_DATA_H
+#define WORD_DATA_H
+
+
+class word_data
+{
+    public:
+        word_data();
+        virtual ~word_data();
+
+    protected:
+
+    private:
+};
+
+#endif // WORD_DATA_H
---
+++ src/word_data.cpp
@@ -0,0 +1,11 @@
+#include "word_data.h"
+
+word_data::word_data()
+{
+    //ctor
+}
+
+word_data::~word_data()
+{
+    //dtor
+}
